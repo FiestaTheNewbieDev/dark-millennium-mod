a7f53070ebeff83ccb37e64123a460cf5cb0642e
Clément FOSSORIER <clementfiesta@gmail.com>
2022-12-29 18:02:57 +0100

    Some blocks added

--- src/main/java/fr/fiesta/dmm/world/block/ModBlocks.java
+++ src/main/java/fr/fiesta/dmm/world/block/ModBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -17,6 +18,15 @@ import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, DMM.MOD_ID);
+
+    public static final RegistryObject<Block> ROCKCRETE = createBlock("rockcrete", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(50f, 600f)), ModCreativeModeTabs.DMM_BLOCKS);
+    public static final RegistryObject<Block> ROCKCRETE_BRICKS = createBlock("rockcrete_bricks", () -> new Block(BlockBehaviour.Properties.copy(ROCKCRETE.get())), ModCreativeModeTabs.DMM_BLOCKS);
+
+    public static final RegistryObject<Block> PLASCRETE = createBlock("plascrete", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(100f, 1200f)), ModCreativeModeTabs.DMM_BLOCKS);
+    public static final RegistryObject<Block> PLASCRETE_BRICKS = createBlock("plascrete_bricks", () -> new Block(BlockBehaviour.Properties.copy(PLASCRETE.get())), ModCreativeModeTabs.DMM_BLOCKS);
+
+    public static final RegistryObject<Block> FERROCRETE = createBlock("ferrocrete", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(250f, 3000f)), ModCreativeModeTabs.DMM_BLOCKS);
+    public static final RegistryObject<Block> FERROCRETE_BRICKS = createBlock("ferrocrete_bricks", () -> new Block(BlockBehaviour.Properties.copy(FERROCRETE.get())), ModCreativeModeTabs.DMM_BLOCKS);
 
     public static final RegistryObject<Block> EMPEROR_IDOL = createBlock("emperor_idol", () -> new EmperorIdol(5, BlockBehaviour.Properties.copy(Blocks.FLOWER_POT)), ModCreativeModeTabs.DMM_BLOCKS);
 
