daaa626fab94c307c3d66b85d215f29ecf606d3d
Clément FOSSORIER <clementfiesta@gmail.com>
2022-12-29 17:58:46 +0100

    Ogryn entity and Civilian entity added

--- src/main/java/fr/fiesta/dmm/world/entity/ModEntityTypes.java
+++ src/main/java/fr/fiesta/dmm/world/entity/ModEntityTypes.java
@@ -2,7 +2,9 @@ package fr.fiesta.dmm.world.entity;
 
 import fr.fiesta.dmm.DMM;
 import fr.fiesta.dmm.world.entity.chaos.CultistEntity;
+import fr.fiesta.dmm.world.entity.imperium.civilian.CivilianEntity;
 import fr.fiesta.dmm.world.entity.imperium.ImperialGuardEntity;
+import fr.fiesta.dmm.world.entity.imperium.OgrynEntity;
 import fr.fiesta.dmm.world.entity.projectile.BoltEntity;
 import fr.fiesta.dmm.world.entity.projectile.LaserBeamEntity;
 import net.minecraft.resources.ResourceLocation;
@@ -18,18 +20,27 @@ public class ModEntityTypes {
 
     public static final RegistryObject<EntityType<LaserBeamEntity>> LASER_BEAM = ENTITY_TYPES.register("laser_beam", () -> EntityType.Builder.<LaserBeamEntity>of(LaserBeamEntity::new, MobCategory.MISC)
             .sized(0.15f, 0.15f)
+            .noSummon()
             .build(new ResourceLocation(DMM.MOD_ID, "laser_beam").toString()));
     public static final RegistryObject<EntityType<BoltEntity>> BOLT = ENTITY_TYPES.register("bolt", () -> EntityType.Builder.<BoltEntity>of(BoltEntity::new, MobCategory.MISC)
             .sized(0.1f, 0.1f)
+            .noSummon()
             .build(new ResourceLocation(DMM.MOD_ID, "bolt").toString()));
 
     public static final RegistryObject<EntityType<CultistEntity>> CULTIST = ENTITY_TYPES.register("cultist", () -> EntityType.Builder.of(CultistEntity::new, MobCategory.CREATURE)
-            .sized(0.6f, 1.8f)
+            .sized(0.65f, 1.90f)
             .build(new ResourceLocation(DMM.MOD_ID, "cultist").toString()));
     public static final RegistryObject<EntityType<ImperialGuardEntity>> IMPERIAL_GUARD = ENTITY_TYPES.register("imperial_guard", () -> EntityType.Builder.of(ImperialGuardEntity::new, MobCategory.CREATURE)
-            .sized(0.6f, 1.8f)
+            .sized(0.65f, 1.90f)
             .build(new ResourceLocation(DMM.MOD_ID, "imperial_guard").toString()));
 
+    public static final RegistryObject<EntityType<OgrynEntity>> OGRYN = ENTITY_TYPES.register("ogryn", () -> EntityType.Builder.of(OgrynEntity::new, MobCategory.CREATURE)
+            .sized(0.65f, 1.90f)
+            .build(new ResourceLocation(DMM.MOD_ID, "ogryn").toString()));
+
+    public static final RegistryObject<EntityType<CivilianEntity>> CIVILIAN = ENTITY_TYPES.register("civilian", () -> EntityType.Builder.of(CivilianEntity::new, MobCategory.CREATURE)
+            .sized(0.65f, 1.90f)
+            .build(new ResourceLocation(DMM.MOD_ID, "civilian").toString()));
 
     public static void register(IEventBus eventBus) {
         ENTITY_TYPES.register(eventBus);
