905f38cfb862c808a8b83d6093d007c930d474af
Clément FOSSORIER <clementfiesta@gmail.com>
2022-12-29 18:02:05 +0100

    Items properties moved into ModItemProperties class

--- src/main/java/fr/fiesta/dmm/DMM.java
+++ src/main/java/fr/fiesta/dmm/DMM.java
@@ -1,18 +1,13 @@
 package fr.fiesta.dmm;
 
 import fr.fiesta.dmm.client.ModKeyBinds;
+import fr.fiesta.dmm.client.renderer.ModItemProperties;
 import fr.fiesta.dmm.client.renderer.entity.*;
 import fr.fiesta.dmm.network.ModPackets;
 import fr.fiesta.dmm.sounds.ModSounds;
 import fr.fiesta.dmm.world.block.ModBlocks;
 import fr.fiesta.dmm.world.entity.ModEntityTypes;
 import fr.fiesta.dmm.world.item.ModItems;
-import net.minecraft.client.KeyMapping;
-import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.client.renderer.item.ItemProperties;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.client.settings.KeyBindingMap;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -39,41 +34,8 @@ public class DMM {
     }
 
     private void clientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            ItemProperties.register(ModItems.POWER_SWORD.get(), new ResourceLocation(MOD_ID, "enable"), (stack, level, player, id) -> {
-                if (stack.hasTag()) {
-                    if (stack.getTag().getBoolean("enable")) {
-                        return 0;
-                    }
-                }
-                return -1;
-            });
-            ItemProperties.register(ModItems.LASGUN.get(), new ResourceLocation(MOD_ID, "has_mag"), (stack, level, player, id) -> {
-                if (stack.hasTag()) {
-                    if (!stack.getTag().getBoolean("hasMag")) {
-                        return 0;
-                    }
-                }
-                return -1;
-            });
-            ItemProperties.register(ModItems.LASPISTOL.get(), new ResourceLocation(MOD_ID, "has_mag"), (stack, level, player, id) -> {
-                if (stack.hasTag()) {
-                    if (!stack.getTag().getBoolean("hasMag")) {
-                        return 0;
-                    }
-                }
-                return -1;
-            });
-            ItemProperties.register(ModItems.BOLT_PISTOL.get(), new ResourceLocation(MOD_ID, "has_mag"), (stack, level, player, id) -> {
-                if (stack.hasTag()) {
-                    if (!stack.getTag().getBoolean("hasMag")) {
-                        return 0;
-                    }
-                }
-                return -1;
-            });
-        });
         ModKeyBinds.init();
+        ModItemProperties.init();
         ModEntityRenderers.init();
     }
 }
