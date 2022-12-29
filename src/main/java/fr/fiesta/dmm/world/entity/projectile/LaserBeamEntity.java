e72ff490101580384cb2a11d7678f6f7007a8e50
Clément FOSSORIER <clementfiesta@gmail.com>
2022-12-29 18:05:12 +0100

    Laser Beam light effect added

--- src/main/java/fr/fiesta/dmm/world/entity/projectile/LaserBeamEntity.java
+++ src/main/java/fr/fiesta/dmm/world/entity/projectile/LaserBeamEntity.java
@@ -7,6 +7,8 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
 
 public class LaserBeamEntity extends CustomProjectileEntity {
@@ -19,22 +21,27 @@ public class LaserBeamEntity extends CustomProjectileEntity {
 
     public LaserBeamEntity(LivingEntity shooter, float attackDamage, Level level) {
         super(ModEntityTypes.LASER_BEAM.get(), shooter, attackDamage, SPEED, level);
-        //this.oldBlockPos = new BlockPos(this.getOnPos().getX(), this.getOnPos().getY(), this.getOnPos().getZ());
+        this.oldBlockPos = new BlockPos(this.getOnPos().getX(), this.getOnPos().getY(), this.getOnPos().getZ());
     }
 
-    /*
     @Override
     public void onProjectileTick() {
-        if (this.level.getBlockState(this.oldBlockPos).getBlock() == Blocks.LIGHT) this.level.setBlock(this.oldBlockPos, Blocks.GLASS.defaultBlockState(), 1);
-        if (this.level.getBlockState(this.getOnPos()).getMaterial() == Material.AIR) this.level.setBlock(this.getOnPos(), Blocks.LIGHT.defaultBlockState(), 1);
+        if (this.level.isClientSide) {
+            if (this.oldBlockPos != null && this.level.getBlockState(this.oldBlockPos).getBlock() == Blocks.LIGHT) this.level.removeBlock(this.oldBlockPos, true);
+            if (this.level.getBlockState(this.getOnPos()).getMaterial() == Material.AIR) this.level.setBlock(this.getOnPos(), Blocks.LIGHT.defaultBlockState(), 1);
+        }
         this.oldBlockPos = new BlockPos(this.getOnPos().getX(), this.getOnPos().getY(), this.getOnPos().getZ());
-        System.out.println(this.oldBlockPos);
     }
-    */
 
     @Override
     public void onHitBlock(BlockHitResult hitResult) {
         super.onHitBlock(hitResult);
         this.level.playSound(null, this.blockPosition(), ModSounds.LASER_BEAM_IMPACT.get(), SoundSource.PLAYERS, 1f, 1f);
+    }
+
+    @Override
+    public void destroy() {
+        if (this.level.isClientSide && this.oldBlockPos != null && this.level.getBlockState(this.oldBlockPos).getBlock() == Blocks.LIGHT) this.level.removeBlock(this.oldBlockPos, true);
+        super.destroy();
     }
 }
