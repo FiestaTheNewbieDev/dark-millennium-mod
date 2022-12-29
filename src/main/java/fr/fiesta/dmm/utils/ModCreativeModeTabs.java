455592ed246695bb034ca26598f996574585e773
Clément FOSSORIER <clementfiesta@gmail.com>
2022-12-29 17:59:36 +0100

    Entities tab added

--- src/main/java/fr/fiesta/dmm/utils/ModCreativeModeTabs.java
+++ src/main/java/fr/fiesta/dmm/utils/ModCreativeModeTabs.java
@@ -1,5 +1,6 @@
 package fr.fiesta.dmm.utils;
 
+import fr.fiesta.dmm.DMM;
 import fr.fiesta.dmm.world.block.ModBlocks;
 import fr.fiesta.dmm.world.item.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
@@ -7,18 +8,24 @@ import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTabs {
 
-    public static final CreativeModeTab DMM_ITEMS = new CreativeModeTab("dmm_items") {
+    public static final CreativeModeTab DMM_ITEMS = new CreativeModeTab(DMM.MOD_ID + ".items") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.BOLT_PISTOL.get());
         }
     };
 
-    public static final CreativeModeTab DMM_BLOCKS = new CreativeModeTab("dmm_blocks") {
+    public static final CreativeModeTab DMM_BLOCKS = new CreativeModeTab(DMM.MOD_ID + ".blocks") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModBlocks.EMPEROR_IDOL.get());
         }
     };
 
+    public static final CreativeModeTab DMM_ENTITIES = new CreativeModeTab(DMM.MOD_ID + ".entities") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.CULTIST_SPAWN_EGG.get());
+        }
+    };
 }
