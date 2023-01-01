1996cfee559efa3afdc467cf48a316fcf9a412c2
Clément FOSSORIER <clementfiesta@gmail.com>
2023-01-02 00:07:43 +0100

    OgrynEntity added

--- src/main/java/fr/fiesta/dmm/world/entity/imperium/OgrynEntity.java
+++ src/main/java/fr/fiesta/dmm/world/entity/imperium/OgrynEntity.java
@@ -1,11 +1,10 @@
 package fr.fiesta.dmm.world.entity.imperium;
 
-import fr.fiesta.dmm.world.item.ModItems;
+import fr.fiesta.dmm.world.entity.chaos.ChaosEntity;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
@@ -19,31 +18,35 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
-public class ImperialGuardEntity extends Animal {
-
-    public ImperialGuardEntity(EntityType<? extends Animal> type, Level level) {
+public class OgrynEntity extends ImperiumEntity {
+    public OgrynEntity(EntityType<? extends Animal> type, Level level) {
         super(type, level);
-        this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(ModItems.CHAINSWORD.get()));
     }
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new MeleeAttackGoal(this, 1, true));
         this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 0.5D));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
         this.targetSelector.addGoal(0, new HurtByTargetGoal(this));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Monster.class, true));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, ChaosEntity.class, true));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Monster.class, true));
         super.registerGoals();
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 20.0f)
-                .add(Attributes.ATTACK_DAMAGE, 1.0f)
-                .add(Attributes.MOVEMENT_SPEED, 0.3F);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 50.0f)
+                .add(Attributes.ATTACK_DAMAGE, 15.0f)
+                .add(Attributes.MOVEMENT_SPEED, 0.25F);
     }
 
     @Override
     public boolean canHoldItem(ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public boolean canTakeItem(ItemStack stack) {
         return true;
     }
 
@@ -51,17 +54,5 @@ public class ImperialGuardEntity extends Animal {
     @Override
     public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob p_146744_) {
         return null;
-    }
-
-    @Override
-    protected SoundEvent getDeathSound() {
-        super.getDeathSound();
-        return SoundEvents.PLAYER_DEATH;
-    }
-
-    @Override
-    protected SoundEvent getHurtSound(DamageSource damageSource) {
-        super.getHurtSound(damageSource);
-        return SoundEvents.PLAYER_HURT;
     }
 }
