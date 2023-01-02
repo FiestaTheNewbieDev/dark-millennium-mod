782f54c761721da009e0c048f06de68f011dd1d6
Clément FOSSORIER <clementfiesta@gmail.com>
2023-01-02 21:56:26 +0100

    CultistEntity can use guns

--- src/main/java/fr/fiesta/dmm/world/entity/chaos/CultistEntity.java
+++ src/main/java/fr/fiesta/dmm/world/entity/chaos/CultistEntity.java
@@ -1,10 +1,16 @@
 package fr.fiesta.dmm.world.entity.chaos;
 
+import fr.fiesta.dmm.world.entity.ai.RangedGunAttackGoal;
+import fr.fiesta.dmm.world.entity.ai.RangedGunAttackMob;
 import fr.fiesta.dmm.world.entity.imperium.ImperiumEntity;
+import fr.fiesta.dmm.world.entity.imperium.imperial_guard.ImperialGuardEntity;
 import fr.fiesta.dmm.world.item.ModItems;
+import fr.fiesta.dmm.world.item.gun.GunItem;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -15,14 +21,29 @@ import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
-public class CultistEntity extends ChaosEntity {
+public class CultistEntity extends ChaosEntity implements RangedGunAttackMob {
+    private final MeleeAttackGoal MELEE_ATTACK_GOAL = new MeleeAttackGoal(this,1, true) {
+        public void stop() {
+            super.stop();
+            CultistEntity.this.setAggressive(false);
+        }
+
+        public void start() {
+            super.start();
+            CultistEntity.this.setAggressive(true);
+        }
+    };
+    private final RangedGunAttackGoal<CultistEntity> RANGED_GUN_ATTACK_GOAL = new RangedGunAttackGoal<CultistEntity>(this, 1.0D, 15.0F);
+
     public CultistEntity(EntityType<? extends Animal> type, Level level) {
         super(type, level);
-        this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(ModItems.CHAINSWORD.get()));
+        this.randomWeapon();
+        this.reassessWeaponGoal();
     }
 
     @Override
@@ -37,6 +58,41 @@ public class CultistEntity extends ChaosEntity {
         super.registerGoals();
     }
 
+    public void randomWeapon() {
+        int max = 3;
+        int random = (int)(Math.random() * (max) + 1);
+        switch (random) {
+            case 1:
+                this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(ModItems.CHAINSWORD.get()));
+                break;
+            case 2:
+                this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(ModItems.LASPISTOL.get()));
+                break;
+            case 3:
+                this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(ModItems.LASGUN.get()));
+                break;
+        }
+    }
+
+    public void reassessWeaponGoal() {
+        this.goalSelector.removeGoal(RANGED_GUN_ATTACK_GOAL);
+        this.goalSelector.removeGoal(MELEE_ATTACK_GOAL);
+        if (this.level != null && !this.level.isClientSide) {
+            ItemStack itemStack = this.getItemInHand(ProjectileUtil.getWeaponHoldingHand(this, item -> item instanceof GunItem));
+            if (itemStack.getItem() instanceof GunItem gun) {
+                int i = 20;
+                if (this.level.getDifficulty() != Difficulty.HARD) {
+                    i = 40;
+                }
+
+                this.RANGED_GUN_ATTACK_GOAL.setFireCooldown(gun.fireCooldown);
+                this.goalSelector.addGoal(0, RANGED_GUN_ATTACK_GOAL);
+            } else {
+                this.goalSelector.addGoal(0, MELEE_ATTACK_GOAL);
+            }
+        }
+    }
+
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 20.0f)
@@ -45,7 +101,26 @@ public class CultistEntity extends ChaosEntity {
     }
 
     @Override
+    public void readAdditionalSaveData(CompoundTag tag) {
+        super.readAdditionalSaveData(tag);
+        this.reassessWeaponGoal();
+    }
+
+    @Override
+    public void setItemSlot(EquipmentSlot slot, ItemStack stack) {
+        super.setItemSlot(slot, stack);
+        if (!this.level.isClientSide) {
+            this.reassessWeaponGoal();
+        }
+    }
+
+    @Override
     public boolean canHoldItem(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public boolean canTakeItem(ItemStack stack) {
         return true;
     }
 
@@ -65,5 +140,12 @@ public class CultistEntity extends ChaosEntity {
     @Override
     public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
         return null;
+    }
+
+    public void fire() {
+        ItemStack itemstack = this.getItemInHand(ProjectileUtil.getWeaponHoldingHand(this, item -> item instanceof GunItem));
+        if (itemstack.getItem() instanceof GunItem gun) {
+            gun.fire(this.getLevel(), this, null);
+        }
     }
 }
