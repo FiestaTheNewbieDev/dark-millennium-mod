5b0f318e14066ccd1089e3f7fde4cc2305ec827e
Clément FOSSORIER <clementfiesta@gmail.com>
2023-01-02 10:43:59 +0100

    LaserBeamEntity model

---
+++ src/main/java/fr/fiesta/dmm/client/model/entity/projectile/LaserBeamModel.java
@@ -0,0 +1,43 @@
+package fr.fiesta.dmm.client.model.entity.projectile;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import fr.fiesta.dmm.DMM;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class LaserBeamModel extends Model {
+    public static final ModelLayerLocation LAYER = new ModelLayerLocation(new ResourceLocation(DMM.MOD_ID, "laser_beam"), "main");
+
+    private final ModelPart main;
+
+    public LaserBeamModel(ModelPart root) {
+        super(RenderType::entityCutout);
+        this.main = root.getChild("main");
+    }
+
+    public static MeshDefinition createMesh() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        PartDefinition main = partdefinition.addOrReplaceChild("main", CubeListBuilder.create().texOffs(0, 0).addBox(0.0F, -1.0F, -8.0F, 0.0F, 2.0F, 16.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-1.0F, 0.0F, -8.0F, 2.0F, 0.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 23.0F, 0.0F));
+        return meshdefinition;
+    }
+
+    public static LayerDefinition createLayer() {
+        return LayerDefinition.create(createMesh(), 64, 64);
+    }
+
+    @Override
+    public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, blue, green, alpha);
+    }
+}
